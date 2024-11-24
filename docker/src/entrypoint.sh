4ef3219535e1fac392904ec9fc311624c05258f7
Franco Sbaffi <francosbaffidev@gmail.com>
2024-11-24 03:56:25 -0300

    Update

--- docker/src/entrypoint.sh
+++ docker/src/entrypoint.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-# Si el repositorio ya existe, hacer pull; de lo contrario, clonarlo
+# Si el repositorio ya existe, actualízalo; si no, clónalo
 if [ -d /app/.git ]; then
     cd /app
     git pull
 else
-    git clone https://gitlab.com/niacuna/tp2-administracion-en-la-nube.git /app
+    git clone https://github.com/FrancoSbaffi/TP02.git /app
     cd /app
     git checkout version-2
 fi
 
-# Copiar el archivo index.html al directorio de Apache
+# Copiar el archivo HTML al directorio de Apache
 cp /app/docker/src/index.html /var/www/html/index.html
 
-# Reemplazar el texto "Hora Actual" con el valor de TITULO
+# Reemplazar el texto "Hora Actual" con el valor de la variable TITULO
 replace=${TITULO:-"Hora Del Mundo"}
 sed -i "s/Hora Actual/${replace}/" /var/www/html/index.html
 
-# Ejecutar el comando CMD (Apache en este caso)
+# Ejecutar el comando CMD (inicializar Apache)
 exec "$@"
+
 
